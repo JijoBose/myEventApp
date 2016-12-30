a4a786d0999e42a4d09a8ab737732dea80457165
JijoBose <bosejijo@gmail.com>
2016-12-30 15:11:31 +0530

    Blog MVC

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161230093629) do
+ActiveRecord::Schema.define(version: 20161230094027) do
+
+  create_table "blogs", force: :cascade do |t|
+    t.integer  "user_id"
+    t.string   "title"
+    t.text     "content"
+    t.string   "slug"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
