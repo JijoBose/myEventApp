dda1d15508bdd9763d3938fc06702dc2f7928003
Jijo Bose <bosejijo@gmail.com>
2017-01-13 18:16:35 +0530

    improved

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   resources :events
   resources :blogs
   devise_for :users
+
+  root 'blogs#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
